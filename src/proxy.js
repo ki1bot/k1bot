e42c4a055fc6e75cb320bf59430f833ac823a421
Pongo <f8goodspoof@gmail.com>
2026-06-01 07:05:11 +0700

    Clear repository contents

--- src/proxy.js
+++
@@ -1,9 +0,0 @@
-import { handleReloadToHomeProxy } from "./middleware/middleware";
-
-export function proxy(request) {
-  return handleReloadToHomeProxy(request);
-}
-
-export const config = {
-  matcher: ["/about", "/experience", "/projects", "/portfolio", "/contact"],
-};
